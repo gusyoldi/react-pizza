1a9a513dc209ef94f21a0eaa1a3fbb02cdf77563
Gus <yoldi.gustavo@gmail.com>
2025-09-01 15:59:56 -0300

    feat: add types

---
+++ src/types/cart.ts
@@ -0,0 +1,7 @@
+export type CartType = {
+  pizzaId: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
+};
---
+++ src/types/order.ts
@@ -0,0 +1,23 @@
+import { CartType } from "./cart";
+
+export type OrderType = {
+  id: string;
+  customer: string;
+  phone: string;
+  address: string;
+  priority: boolean;
+  status: "preparing" | "on the way" | "delivered";
+  estimatedDelivery: string;
+  cart: CartType[];
+  position: string;
+  orderPrice: number;
+  priorityPrice: number;
+};
+
+export type OrderFormData = {
+  customer: string;
+  phone: string;
+  address: string;
+  cart: string;
+  priority?: string;
+};
