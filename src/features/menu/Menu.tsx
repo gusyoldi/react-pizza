6e30bf5414c269c75a9301b9208f706e2b310703
Gus <yoldi.gustavo@gmail.com>
2025-09-01 16:04:31 -0300

    feat: add loaders

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,11 +1,9 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
-
   return (
     <>
       <h1>Menu</h1>
