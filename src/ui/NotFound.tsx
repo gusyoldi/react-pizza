604a1df03aa95acd7af709dc747c2b5a98375cdc
Gus <yoldi.gustavo@gmail.com>
2025-09-02 14:23:28 -0300

    chore(deploy): create manually 404 file

--- src/ui/NotFound.tsx
+++ src/ui/NotFound.tsx
@@ -8,13 +8,13 @@ function NotFound() {
   const errorMessage =
     error && typeof error === 'object' && 'message' in error
       ? (error as { message: string }).message
-      : 'Unknown error';
+      : 'Error desconocido';
 
   return (
     <div>
       <h1>Algo salió mal 😢</h1>
       <p>{errorMessage}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <button onClick={() => navigate(-1)}>&larr; Volver</button>
     </div>
   );
 }
