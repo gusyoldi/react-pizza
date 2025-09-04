697b883bd2fd76805a9b6a8388a768f0ada4a562
Gus <yoldi.gustavo@gmail.com>
2025-09-04 05:35:46 -0300

    style: style cart features

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -1,10 +1,12 @@
 import { formatCurrency } from '../../utils/helpers';
-
+import Button from './../../ui/Button';
 interface CartItemProps {
   item: {
     name: string;
     quantity: number;
     totalPrice: number;
+    pizzaId: number;
+    unitPrice: number;
   };
 }
 
@@ -12,12 +14,13 @@ function CartItem({ item }: CartItemProps) {
   const { name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Borrar</Button>
       </div>
     </li>
   );
