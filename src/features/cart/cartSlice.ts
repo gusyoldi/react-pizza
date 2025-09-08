aaae12f065021908a396cfae8528f48b1cc33f5c
Gus <yoldi.gustavo@gmail.com>
2025-09-08 11:54:32 -0300

    feat: add cart store methods

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../store/store';
 import { CartType } from '../../types/cart';
 
 type CartState = { cart: CartType[] };
@@ -19,7 +20,6 @@ const cartSlice = createSlice({
     },
     increaseItemQuantity(state, action) {
       const item = state.cart.find((item) => item.pizzaId === action.payload)!;
-
       item.quantity++;
       item.totalPrice = item.unitPrice * item.quantity;
     },
@@ -28,6 +28,8 @@ const cartSlice = createSlice({
 
       item.quantity--;
       item.totalPrice = item.unitPrice * item.quantity;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -44,3 +46,14 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state: RootState) => state.cartStore.cart;
+
+export const getTotalCartQuantity = (state: RootState) =>
+  state.cartStore.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state: RootState) =>
+  state.cartStore.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id: number) => (state: RootState) =>
+  state.cartStore.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
